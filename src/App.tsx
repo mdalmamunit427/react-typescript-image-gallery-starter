3d25b4c8e4b16b5375daeab565f23bb5935f0a9f
mdalmamunit427 <mdalmamunit427@gmail.com>
2023-12-11 11:22:32 +0600

    starter files

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div>
+      <h1 className="h-screen flex items-center justify-center text-3xl font-bold">
+        Image Gallery <span className="text-blue-600 ml-2">  Starter Files!</span>
+      </h1>
+    </div>
+  );
+};
+
+export default App;
